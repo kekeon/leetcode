31ed64c439e58f8ad0b5bf0893e732997ba3c52e
kekeon <liuke095@gmail.com>
2019-01-15 00:57:25 +0800

    bracket valid

--- bracketValid.go
+++ bracketValid.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"strings"
 	"fmt"
+	"strings"
 )
 
 // 20. 有效的括号
 func bracketValid(s string) bool {
 
+	isValid := false
 	if len(s)%2 != 0 {
-		return false
+		return isValid
 	}
 	m := make(map[string]string)
 
@@ -18,53 +19,37 @@ func bracketValid(s string) bool {
 	m["("] = ")"
 
 	i := 0
-	isValid := false
+
 	for {
-		if i >= len(s) {
+		l := len(s)
 
-			if 0 == len(s) {
-				isValid = true
-			}
 
+		if l -1 <= i {
 			break
-		} else {
-			// 判断 相邻一个是否，满足
-			a := string(s[i])
-
-			if i + 1 < len(s) && m[a] == string(s[i+1]) {
-
-				var cs string
-				if i > 0 {
-					cs = s[0:i]
-					s = s[i:]
-				}
-
-				s = strings.Replace(s, a, "", 1)
-				s = strings.Replace(s, string(s[i]), "", 1)
-				s = cs + s
-				i = 0
-				// 判断成对字符串
-			} else if  i + 3 < len(s) && m[a] == string(s[i+3]) && string(s[i+1]) == string(s[i+2]) {
-				s = strings.Replace(s, a, "", 1)
-				s = strings.Replace(s, string(s[i+2]), "", 1)
-				i = 0
-			} else {
-				i++
-			}
+		}
 
 
-			fmt.Println(s)
-			fmt.Println(i)
+		if m[string(s[i])] == string(s[i+1]) {
+			sArr := strings.Split(s, "")
+
+			sArr[i] = ""
+			sArr[i+1] = ""
+			s = strings.Join(sArr, "")
+			i = 0
+		}else {
+			i++
 		}
 	}
 
-	return isValid
+	if len(s) != 0 {
+		return false
+	}else {
+		return true
+
+	}
 }
 
 func main() {
-	fmt.Println("===")
-	//v := "123"[0:1]
-
-	v := bracketValid("[()](())")
+	v := bracketValid("[()](()(})")
 	fmt.Println(v)
 }
