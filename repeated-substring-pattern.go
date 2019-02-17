e709b0298919348fdcb5db9e264b1d7cebfd7c92
kekeon <liuke095@gmail.com>
2019-02-17 15:17:34 +0800

    repeated substring pattern

---
+++ repeated-substring-pattern.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func repeatedSubstringPattern(s string) bool {
+
+	if len(s) == 1 {
+		return false
+	}
+
+	l := len(s)
+
+	for i:= 1; i<=l / 2; i++ {
+		sub := s[0:i]
+
+		if l%len(sub) == 0 && strings.Repeat(sub,l / len(sub)) == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	v := repeatedSubstringPattern("abaabaabac")
+	fmt.Println(v)
+}
