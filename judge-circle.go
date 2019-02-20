96261c63ee84672903c455f44e98020b724330bf
kekeon <liuke095@gmail.com>
2019-02-20 23:05:03 +0800

    update

--- judge-circle.go
+++ judge-circle.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func judgeCircle(moves string) bool {
 
-	point := [2]int{0,0}
+	point := [2]int{0, 0}
 
-	for i := 0; i < len(moves);i++  {
+	for i := 0; i < len(moves); i++ {
 		switch string(moves[i]) {
 
 		case "U":
