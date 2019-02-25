9222dc5a7bbdba197b6d33f9b47aad31948cfd6e
kekeon <liuke095@gmail.com>
2019-02-26 00:50:12 +0800

    buddy strings

--- buddy-strings.go
+++ buddy-strings.go
@@ -8,10 +8,30 @@ func buddyStrings(A string, B string) bool {
 		return false
 	}
 
+	if len(B) == 2 && B[0] != B[1] &&A[0] == B[0] && A[1] == B[1] {
+		return false
+	}
 
+
+	m := map[string]bool{}
+
+	for _, k := range A {
+		m[string(k)] = true
+	}
+
+	for _, k := range B {
+		if !m[string(k)] {
+			return false
+		}else {
+			m[string(k) + "-"] = false
+		}
+	}
+
+
+	return true
 }
 
 func main() {
-	v := buddyStrings("", "")
+	v := buddyStrings("aa", "aa")
 	fmt.Println(v)
 }
