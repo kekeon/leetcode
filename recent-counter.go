961d41998cd3f723fd1bda8ab96b18f6fcde8ef5
kekeon <liuke095@gmail.com>
2019-03-06 00:26:59 +0800

    recent count

--- recent-counter.go
+++ recent-counter.go
@@ -1,19 +1,34 @@
 package main
 
-type RecentCounter struct {
+import "fmt"
 
+type RecentCounter struct {
+	ints []int
 }
 
 
 func Constructor() RecentCounter {
-
+	return RecentCounter {
+		ints: []int{},
+	}
 }
 
 
 func (this *RecentCounter) Ping(t int) int {
-
+	this.ints = append(this.ints, t)
+	fmt.Println()
+	for this.ints[0] < t-3000{
+		this.ints = this.ints[1:]
+	}
+	
+	return  len(this.ints)
 }
 
 func main() {
-	
+
+		t := 1
+	 obj := Constructor()
+	 v := obj.Ping(t)
+
+	fmt.Println(v)
 }
