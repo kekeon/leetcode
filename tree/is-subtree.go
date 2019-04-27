66075442a85dc1dfd79d9067a71f446a112ad150
kekeon <liuke095@gmail.com>
2019-04-27 23:23:31 +0800

    update is sub tree

--- tree/is-subtree.go
+++ tree/is-subtree.go
@@ -5,11 +5,11 @@ func isSubtree(s *TreeNode1, t *TreeNode1) bool {
 		return true
 	}
 
-	if s == nil && t != nil {
+	if s == nil || t == nil {
 		return false
 	}
 
-	return sameTree(s, t) || s != nil && t != nil && isSubtree(s.Left, t.Left) || s != nil && t != nil && isSubtree(s.Right, t.Right)
+	return sameTree(s, t) || s != nil && t != nil && isSubtree(s.Left, t) || s != nil && t != nil && isSubtree(s.Right, t)
 }
 
 func sameTree(s, t *TreeNode1) bool {
