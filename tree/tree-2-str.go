a2a48d29e159825ac91555ccd02a9bf5d5b444bd
kekeon <liuke095@gmail.com>
2019-04-29 01:01:29 +0800

    add annotate

--- tree/tree-2-str.go
+++ tree/tree-2-str.go
@@ -10,14 +10,19 @@ func tree2str(t *TreeNode1) string {
 		return ""
 	}
 
+	// 1. 总体思路：t.val + (t.Left) + (t.Right)
+
 	if t.Left == nil && t.Right == nil {
 
 		return strconv.Itoa(t.Val)
 	}
 
+	// 2. 处理特殊case, 右子树为nil, 则右() 可省略
 	if t.Left != nil && t.Right == nil {
 
 		return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")"
+
+	// 3. 左子树为nil, 则左()不 可省略
 	} else if t.Left == nil && t.Right != nil {
 
 		return strconv.Itoa(t.Val) + "()" + "(" + tree2str(t.Right) + ")"
