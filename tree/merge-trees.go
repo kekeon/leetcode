6936775faf8ba3d556bee2c7ee75c30701afe334
kekeon <liuke095@gmail.com>
2019-05-02 23:55:40 +0800

    format code

--- tree/merge-trees.go
+++ tree/merge-trees.go
@@ -2,12 +2,11 @@ package tree
 
 func mergeTrees(t1 *TreeNode1, t2 *TreeNode1) *TreeNode1 {
 
-
 	if t1 == nil && t2 == nil {
 		return t1
-	}else if t1 == nil && t2!= nil {
+	} else if t1 == nil && t2 != nil {
 		&t1 = &t2
-	}else if t1 != nil && t2 != nil {
+	} else if t1 != nil && t2 != nil {
 		t1.Val = t1.Val + t2.Val
 		t1.Left = mergeTrees(t1.Left, t2.Left)
 		t1.Right = mergeTrees(t1.Right, t2.Right)
