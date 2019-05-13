1440b84c2b83667fb8240cbf39760512aad6ca15
kekeon <liuke095@gmail.com>
2019-05-14 00:30:16 +0800

    sum root to leaf

--- tree/sum-root-to-leaf.go
+++ tree/sum-root-to-leaf.go
@@ -1,5 +1,23 @@
 package tree
 
-func sumRootToLeaf(root *TreeNode) int {
+var sum int
+func sumRootToLeaf(root *TreeNode1) int {
+	return dfs(root, 0)
+}
 
+func dfs(node *TreeNode1, v int) int {
+	v = v << 1 | node.Val
+	if node.Left == nil && node.Right == nil {
+		sum  += v
+		return sum
+	}
+
+	if node.Left != nil {
+		dfs(node.Left, v)
+	}
+
+	if node.Right != nil {
+		dfs(node.Right, v)
+	}
+	return sum
 }
