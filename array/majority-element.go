20a53562901428513bae6d6895abba72c48e8c7c
kekeon <liuke095@gmail.com>
2019-05-17 00:29:16 +0800

    tow sum

--- array/majority-element.go
+++ array/majority-element.go
@@ -4,10 +4,10 @@ func majorityElement(nums []int) int {
 
 	m := map[int]int{}
 
-	l := float64(len(nums)) / 2
+	l := float32(len(nums)) / 2
 	for _, v := range nums {
 		m[v]++
-		if float64(m[v]) > l {
+		if float32(m[v]) > l {
 			return v
 		}
 	}
