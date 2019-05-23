9c84e1bf633689aed02d468b4d88167ba5a7a3ec
kekeon <liuke095@gmail.com>
2019-05-24 00:26:51 +0800

    maximum product

---
+++ array/maximum-product.go
@@ -0,0 +1,20 @@
+package array
+
+import (
+	"sort"
+	"fmt"
+)
+
+func maximumProduct(nums []int) int {
+	sort.Ints(nums)
+	fmt.Println(nums)
+	l := len(nums)
+
+	if nums[0] > 0 || nums[l-1] <= 0 {
+		return nums[l-3] * nums[l-2] * nums[l-1]
+	}else  if nums[0] * nums[1] > nums[l-3] * nums[l-2] {
+		return nums[0] * nums[1] * nums[l-1]
+	}else {
+		return nums[l-3] * nums[l-2] * nums[l-1]
+	}
+}
