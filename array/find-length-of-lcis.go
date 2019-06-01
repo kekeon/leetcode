4f21774c2d913420eada787183953d1fcfac37e8
kekeon <liuke095@gmail.com>
2019-06-01 23:47:11 +0800

    find length of LCTIS

---
+++ array/find-length-of-lcis.go
@@ -0,0 +1,29 @@
+package array
+
+func findLengthOfLCIS(nums []int) int {
+
+	l := len(nums)
+
+	if l == 0 {
+		return 0
+	}
+
+	if l == 1 {
+		return 1
+	}
+
+	currentL := 1
+	lastL := 1
+
+	for i := 1; i < l ; i++ {
+		if nums[i] > nums [i - 1]  {
+			currentL ++
+		}else if currentL > lastL {
+			lastL = currentL
+		} else {
+			currentL = 1
+		}
+	}
+
+	return lastL
+}
