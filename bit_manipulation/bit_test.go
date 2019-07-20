f03c63f7976324d276be9b018280a7d53c005d17
kekeon <liuke095@gmail.com>
2019-07-20 23:55:44 +0800

    update

--- bit_manipulation/bit_test.go
+++ bit_manipulation/bit_test.go
@@ -9,3 +9,9 @@ func TestSingleNumber(t *testing.T) {
 	v := singleNumber([]int{4,1,2,1,2,4,3})
 	fmt.Println(v)
 }
+
+func TestReverseBits( t *testing.T) {
+
+	v := reverseBits( 238)
+	fmt.Println(v)
+}
---
+++ bit_manipulation/reverse-bits.go
@@ -0,0 +1,15 @@
+package bit_manipulation
+
+func reverseBits(num uint32) uint32 {
+
+	var ans uint32
+	for i := 0; i < 8; {
+
+		ans <<= 1
+		ans |= num&1
+		num >>= 1
+		i++
+	}
+
+	return ans
+}
