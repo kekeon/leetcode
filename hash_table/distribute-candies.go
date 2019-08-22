4f09b930f30a5dfffaf208dd4c5e689f8d231e87
kekeon <liuke095@gmail.com>
2019-08-23 00:20:07 +0800

    distribute candies

---
+++ hash_table/distribute-candies.go
@@ -0,0 +1,21 @@
+package hash_table
+
+func distributeCandies(candies []int) int {
+	l := len(candies)
+
+	m := map[int]int{}
+	countKey := 0
+
+	for _, v := range candies {
+		m[v]++
+		if m[v] == 1 {
+			countKey++
+		}
+	}
+
+	if l / 2 >= countKey {
+		return countKey
+	}
+
+	return l / 2
+}
