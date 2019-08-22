19953f5770e98a18ca604e6c6428b669bf063a22
kekeon <liuke095@gmail.com>
2019-08-23 00:43:33 +0800

    find lhs

--- hash_table/hash_test.go
+++ hash_table/hash_test.go
@@ -11,8 +11,14 @@ func TestLongestPalindrome(t *testing.T) {
 	fmt.Println(v)
 }
 
-func ()  {
+func TestFindWords(t *testing.T)  {
 	s := []string{"Hello", "Alaska", "Dad", "Peace"}
 	v := findWords(s)
+	fmt.Println(v)
+}
+
+func TestFindLHS(t *testing.T) {
+	s := []int{1,1,1,1,1}
+	v := findLHS(s)
 	fmt.Println(v)
 }
