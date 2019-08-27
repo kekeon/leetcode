3763c35f443f6ab9adecb80abea324e454cd7e32
kekeon <liuke095@gmail.com>
2019-08-27 23:46:53 +0800

    format code

--- hash_table/my-hash-set.go
+++ hash_table/my-hash-set.go
@@ -6,23 +6,19 @@ type MyHashSet struct {
 	set map[int]int
 }
 
-
 /** Initialize your data structure here. */
 func Constructor() MyHashSet {
 
 	return MyHashSet{set: map[int]int{}}
 }
 
-
-func (this *MyHashSet) Add(key int)  {
+func (this *MyHashSet) Add(key int) {
 	this.set[key] ++
 }
 
-
-func (this *MyHashSet) Remove(key int)  {
+func (this *MyHashSet) Remove(key int) {
 	delete(this.set, key)
 }
-
 
 /** Returns true if this set contains the specified element */
 func (this *MyHashSet) Contains(key int) bool {
