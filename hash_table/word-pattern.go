0a7f542d7bfc6b0df143aad8882172cf1d6d8922
kekeon <liuke095@gmail.com>
2019-09-14 16:39:08 +0800

    word pattern

---
+++ hash_table/word-pattern.go
@@ -0,0 +1,25 @@
+package hash_table
+
+import "strings"
+
+func wordPattern(pattern string, str string) bool {
+	pm := map[string]int{}
+	sm := map[string]int{}
+
+	strs := strings.Split(str, " ")
+
+	if len(strs) != len(pattern) {
+		return false
+	}
+
+	for i:=0; i < len(pattern); i++ {
+		pm[string(pattern[i])] += i + 1
+		sm[strs[i]] += i + 1
+
+		if pm[string(pattern[i])] != sm[strs[i]] {
+			return false
+		}
+	}
+
+	return true
+}
