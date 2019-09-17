270d5d3cfcc0a9ccc4fda579f022691292ce159c
kekeon <liuke095@gmail.com>
2019-09-18 00:03:04 +0800

    convert title

--- myMath/convert-to-title.go
+++ myMath/convert-to-title.go
@@ -4,20 +4,14 @@ func convertToTitle(n int) string {
 
 	m := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
-	if n <= 26 {
-		return m[n-1]
+	i := 0
+
+	s := ""
+	for n > 0 {
+		i = ( n-1 ) % 26
+		s = m[i] + s
+		n = (n -1) / 26
 	}
 
-	b := (n - n % 26) / 26
-
-	r := (n - b - 1) % 25
-
-	if b == 0 {
-
-		return m[b] + m[((n - 1) % 26) - 1]
-
-	} else {
-		return m[b - 1] + m[r]
-	}
-
+	return s
 }
