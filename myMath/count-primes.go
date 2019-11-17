25d2635ed082401feff8962c9fe90d5dac147e12
kekeon <liuke095@gmail.com>
2019-11-17 23:49:13 +0800

    count primes

---
+++ myMath/count-primes.go
@@ -0,0 +1,17 @@
+package myMath
+
+func countPrimes(n int) int {
+	c := 0
+	arr := make([]bool, n)
+
+	for i := 2; i < n; i++ {
+		if (!arr[i-1]) {
+			c++
+			for j := i*i; j <=n; j += i {
+				arr[j-1] = true
+			}
+		}
+	}
+
+	return c
+}
---
+++ myMath/reach-number.go
@@ -0,0 +1,5 @@
+package myMath
+
+func reachNumber(target int) int {
+
+}
